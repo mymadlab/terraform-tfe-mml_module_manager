3545e1a31465a59bea844654c3030d0a5b531ff2
biggiebk <biggiebk@mymadlab.net>
2024-04-06 18:41:37 -0700

    Cleanup and standards updates

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# terraform
+# mml_module_manager
+
+## Description
+
+Responsible for creating, updating, and deleting workspaces in terraform cloud.
+
+## Usage
+
+```HCL
+module "tfe_module_manager" {
+  source  = "app.terraform.io/mymadlab/mml_module_manager/tfe"
+  version = "~>1.0.0"
+
+  module_name       = var.name
+  tfe_org           = var.tfe_org
+  vcs_provider_name = var.vcs_provider_name
+  github_org        = var.github_org
+
+}
+```
+
+## Dependencies
+
+- [hashicorp/tfe](https://registry.terraform.io/providers/hashicorp/tfe/latest): >=0.52.0
--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
-variable "module_name" {
-  description = "Name of the module"
-  type        = string
-  nullable    = false
-}
-
 variable "github_org" {
-  type = string
-}
-
-variable "vcs_provider_name" {
-  description = "Name of the OATH VCS Provider"
+  description = "The name of the github organization."
   type        = string
-  nullable    = false
 }
 
 variable "tfe_org" {
-  description = "Name of the Terraform organization"
+  description = "Name of the Terraform organization."
+  type        = string
+  nullable    = false
+}
+
+variable "module_name" {
+  description = "Name of the module."
+  type        = string
+  nullable    = false
+}
+
+variable "vcs_provider_name" {
+  description = "Name of the OATH VCS Provider."
   type        = string
   nullable    = false
 }
